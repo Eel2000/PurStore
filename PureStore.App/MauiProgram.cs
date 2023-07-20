965b0c412c9edb8d0bbdb77a89ca3901b1e998d7
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2023-07-20 22:51:10 +0100

    fonts added

--- PureStore.App/MauiProgram.cs
+++ PureStore.App/MauiProgram.cs
@@ -13,6 +13,9 @@ namespace PureStore.App
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Font Awesome 6 Brands-Regular-400.otf", "faBrandRegular");
+                    fonts.AddFont("Font Awesome 6 Free-Regular-400.otf", "faRegular");
+                    fonts.AddFont("Font Awesome 6 Free-Solid-900.otf", "faSolid");
                 });
 
 #if DEBUG
