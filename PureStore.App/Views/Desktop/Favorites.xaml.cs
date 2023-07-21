7ed93a3680ad42023189ebe687c0e318a5a7da39
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2023-07-21 10:55:47 +0100

    left pan nav

---
+++ PureStore.App/Views/Desktop/Favorites.xaml.cs
@@ -0,0 +1,9 @@
+namespace PureStore.App.Views.Desktop;
+
+public partial class Favorites : ContentPage
+{
+	public Favorites()
+	{
+		InitializeComponent();
+	}
+}
---
+++ PureStore.App/Views/Desktop/User.xaml.cs
@@ -0,0 +1,9 @@
+namespace PureStore.App.Views.Desktop;
+
+public partial class User : ContentPage
+{
+	public User()
+	{
+		InitializeComponent();
+	}
+}
