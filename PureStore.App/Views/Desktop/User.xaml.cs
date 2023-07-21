fb452927c3be160e136c4610335bab68403e22e1
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2023-07-21 20:19:59 +0100

    minor update

--- PureStore.App/Views/Desktop/User.xaml.cs
+++ PureStore.App/Views/Desktop/User.xaml.cs
@@ -2,8 +2,13 @@ namespace PureStore.App.Views.Desktop;
 
 public partial class User : ContentPage
 {
-	public User()
-	{
-		InitializeComponent();
-	}
+    public User()
+    {
+        InitializeComponent();
+    }
+
+    private void btnChangeTheme_Clicked(object sender, EventArgs e)
+    {
+        App.Current.UserAppTheme = AppTheme.Dark;
+    }
 }
