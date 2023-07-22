6a73a34185930fc0d4acd9a2144eeaa77bc84b39
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2023-07-22 13:03:50 +0100

    Finanlize the dashboard ui

--- PureStore.App/App.xaml.cs
+++ PureStore.App/App.xaml.cs
@@ -12,5 +12,28 @@
             MainPage = new AppShell();
 #endif
         }
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            window.MinimumHeight = 820;
+            window.MinimumWidth = 1220;
+            window.Title = "All Store App";
+
+            return window;
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+            UserAppTheme = AppTheme.Dark;
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            UserAppTheme = AppTheme.Dark;
+        }
     }
 }
