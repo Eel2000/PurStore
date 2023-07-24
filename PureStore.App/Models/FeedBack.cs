48be8590497c7cf5a4f76b6fd0a543151276dabd
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2023-07-24 09:13:38 +0100

    user feedback template

---
+++ PureStore.App/Models/FeedBack.cs
@@ -0,0 +1,11 @@
+﻿namespace PureStore.App.Models
+{
+    public class FeedBack
+    {
+        public Guid Id { get; set; }
+        public string Content { get; set; }
+        public string Username { get; set; }
+        public double Rating { get; set; }
+        public DateOnly PublishedAt { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+    }
+}
--- PureStore.App/ViewModels/Desktop/ViewAppPageViewModel.cs
+++ PureStore.App/ViewModels/Desktop/ViewAppPageViewModel.cs
@@ -1,11 +1,39 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using PureStore.App.Models;
+using System.Collections.ObjectModel;
 
 namespace PureStore.App.ViewModels.Desktop
 {
     public partial class ViewAppPageViewModel : ObservableObject
     {
 
+        [ObservableProperty]
+        private ObservableCollection<FeedBack> _feedBacks;
+
+
+        public ViewAppPageViewModel()
+        {
+            LoadDataAsync();
+        }
+
+        private void LoadDataAsync()
+        {
+            var rnd = new Random(1);
+            FeedBacks = new();
+            for (int i = 0; i < 10; i++)
+            {
+                FeedBack feed = new()
+                {
+                    Id = Guid.NewGuid(),
+                    Content = "Awesome app, I've been enjoying it",
+                    Username = "User" + i,
+                    Rating = rnd.Next(1, 5),
+                };
+
+                FeedBacks.Add(feed);
+            }
+        }
 
         [RelayCommand]
         Task Back()
