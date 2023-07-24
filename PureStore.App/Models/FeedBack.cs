33dcc3a685b43d38db7170af3dd34666985a3e7c
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2023-07-24 14:20:45 +0100

    finalize the ui of details/download page

--- PureStore.App/Models/FeedBack.cs
+++ PureStore.App/Models/FeedBack.cs
@@ -7,5 +7,18 @@
         public string Username { get; set; }
         public double Rating { get; set; }
         public DateOnly PublishedAt { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+
+        public string Initials
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(Username))
+                {
+                    return string.Empty;
+                }
+
+                return Username.Remove(2, Username.Length - 2).ToUpper();
+            }
+        }
     }
 }
