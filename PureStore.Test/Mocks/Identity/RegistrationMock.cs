da06a59b45454fb6b1d07d85e9a316ee8b1cba88
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2023-08-05 18:53:36 +0100

    #9 registration proc unit test

---
+++ PureStore.Test/Mocks/Identity/RegistrationMock.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PureStore.Test.Mocks.Identity
+{
+    public static class RegistrationMock
+    {
+        public static RegisterDTO Register()
+            => new("test", "test", "test@gmail.com");
+
+        public static AuthResponse ResgistrationResponse()
+        {
+            return new()
+            {
+                Username = "test",
+                Email = "test",
+                Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+            };
+        }
+    }
+}
