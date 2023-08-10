f519c46e3a7ebd3fd96cc47f0802d14498d0483d
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2023-08-10 12:18:21 +0100

    #7  remove command unit test

---
+++ PureStore.Test/Mocks/Uploads/UploadAppMock.cs
@@ -0,0 +1,30 @@
+﻿using AutoBogus;
+using Bogus;
+using PureStore.Domain.Entities;
+using ServiceStack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PureStore.Test.Mocks.Uploads
+{
+    internal class UploadAppMock
+    {
+
+        public static UploadedApplication GetSingle()
+        {
+            var personFaker = new AutoFaker<UploadedApplication>()
+                      .Configure(builder =>
+                      {
+                          builder.WithRepeatCount(1);
+                      })
+                      .RuleFor(fake => fake.Id, fake => fake.GetId().ToString())
+                      .RuleFor(fake => fake.ImageUrl, fake => fake.Image.ToString())
+                      .Generate();
+
+            return personFaker;
+        }
+    }
+}
